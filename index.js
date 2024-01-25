c66c9a6624f8d85c74631b931791a42fe2fcd3a3
Marcelo Camilo Gomes <marcelocamilogomes@gmail.com>
2024-01-25 18:21:40 -0300

    Testando upload file local no servidor da music.ai

--- index.js
+++ index.js
@@ -1,19 +1,75 @@
 import Moises from "moises/sdk.js"
+import axios from 'axios';
+import express from 'express';
+import path from 'path';
+import fs from 'fs';
 
 const moises = new Moises({ apiKey: "b8b81646-c5e3-4d79-ab89-90701448c70d" })
 
-import express from 'express';
-import path from 'path';
 
-const apiUrl = 'https://api.music.ai/api/job';
+const apiUrlJob = 'https://api.music.ai/api/job';
+const apiUrlUpload = 'https://api.music.ai/api/upload';
 const apiKey = 'b8b81646-c5e3-4d79-ab89-90701448c70d'
 const urlCarelessWhisper = 'public/samples/vlc-record-2024-01-24-17h34m27s-Careless Whisper-.mp3'
 
 const app = express();
 const port = 3000;
 
+axios({
+    method: 'get',
+    url: apiUrlUpload,
+    headers: {
+      'Authorization': apiKey,
+    },
+  })
+    .then(response => {
+      const uploadUrl = response.data.uploadUrl;
+      const downloadUrl = response.data.downloadUrl;
+  
+      console.log('Upload URL:', uploadUrl);
+      console.log('Download URL:', downloadUrl);
+  
+      return axios({
+        method: 'put',
+        url: uploadUrl,
+        headers: {
+          'Content-Type': 'audio/mpeg',
+        },
+        data: fs.readFileSync(urlCarelessWhisper),
+      });
+    })
+    .then(uploadResponse => {
+      console.log('File uploaded successfully :');
+      // console.log(uploadResponse);
+  
+      // Step 3: Using the file
+      const inputUrl = downloadUrl; // Use the downloadUrl obtained from step 1
+  
+      return axios({
+        method: 'post',
+        url: apiUrlJob,
+        headers: {
+          'Authorization': apiKey,
+          'Content-Type': 'application/json',
+        },
+        data: {
+          name: 'Workflow wf-separacao-vocal-instrumento - demo.ogg',
+          workflow: 'wf-separacao-vocal-instrumento',
+          params: {
+            inputUrl: inputUrl,
+          },
+        },
+      });
+    })
+    .then(jobResponse => {
+      console.log('Job submitted successfully');
+      console.log('Job response:', jobResponse.data);
+    })
+    .catch(error => {
+      console.error('Error:', error.message);
+    });
+
 async function jobMoises () {
-    
     const downloadUrl = await moises.uploadFile(urlCarelessWhisper)
 
     const jobId = await moises.addJob(
