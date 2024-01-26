0d6b3d6a2f757f01f90aed60e0822d95485f413a
lisperey <pilrss16@gmail.com>
2024-01-26 02:05:31 -0300

    Init na tela inicial

--- Front-end/src/pages/Home/index.jsx
+++ Front-end/src/pages/Home/index.jsx
@@ -1,12 +1,46 @@
 import React, { memo } from "react";
-import './Home.css';
+import * as S from './styles';
+import logo from './../../assets/Logo-symphonIA.svg'
+
 // import Icon from "./Icon.jsx"
 const Home = () => {
   
     return (
-      <div>
-        
-      </div>
+      <S.Conteiner>
+      <S.WrapperLogo>
+        <img src={logo} alt="logo"></img>
+      </S.WrapperLogo>
+      <S.WrapperMain>
+        <S.WrapperAvatar>
+          
+        </S.WrapperAvatar>
+        <S.WrapperUser>
+        <S.ConteinerText>
+            <S.TitleH2>
+            Perfil anônimo
+            </S.TitleH2>
+            <S.subtle>
+            Escolha seu username e seu avatar e comece a jogar!
+            </S.subtle>
+            <S.Input/>
+            <S.ContainerButtonCriarSala>
+              <S.ButtonCriarSala>
+                Criar Sala
+              </S.ButtonCriarSala>
+            </S.ContainerButtonCriarSala>
+          </S.ConteinerText>
+          
+        </S.WrapperUser>
+      </S.WrapperMain>
+      <S.WrapperAction>
+        <S.ButtonLogar>
+          Logar
+        </S.ButtonLogar>
+        <S.ButtonCadastrar>
+          Cadastre-se
+        </S.ButtonCadastrar>
+      </S.WrapperAction>
+      </S.Conteiner>
     );
   }
 
---
+++ Front-end/src/pages/Home/styles.js
@@ -0,0 +1,171 @@
+import styled from 'styled-components';
+
+export const Conteiner = styled.div`
+    display: flex;
+    width: 1441px;
+    height: 100%;
+    flex-shrink: 0;
+    padding: 24px;
+    flex-direction: column;
+    justify-content: start; /* Alterado de center para flex-start */
+    align-items: center;
+    
+
+`;
+
+export const WrapperLogo = styled.div`
+    
+   width: 903px;
+    height: 162px;
+    flex-shrink: 0;
+     
+  
+`;
+export const WrapperMain = styled.div`
+    margin-top: 81px;
+    display: flex;
+    width: 727px;
+    height: 308px;
+    padding: 24px;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    gap: 16px;
+    flex-shrink: 0;
+    border-radius: 8px;
+    border: 1px solid #CBD5E1;
+    background: rgba(255, 255, 255, 0.50);
+  
+`;
+
+export const WrapperAction = styled.div`
+    margin-top: 16px;
+    display: flex;
+    width: 727px;
+    flex-direction: row;
+    justify-content: flex-end;
+    align-items: self-end;
+    gap: 16px;
+
+  
+`;
+
+export const ButtonLogar = styled.button`
+    display: inline-flex;
+    padding: 8px 16px;
+    justify-content: center;
+    align-items: center;
+    gap: 10px;
+    border-radius: 6px;
+    background: var(--slate-900, #023059);
+    color: #FFF;
+    font-family: Inter;
+    font-size: 14px;
+    font-style: normal;
+    font-weight: 500;
+    line-height: 24px; 
+`;
+export const ButtonCadastrar = styled.button`
+    display: inline-flex;
+    padding: 8px 16px;
+    justify-content: center;
+    align-items: center;
+    gap: 10px;
+    border-radius: 6px;
+    border: 1px solid var(--slate-200, #96ADC6);
+    background: var(--slate-50, #E6EBF1);
+    color: #0F172A;
+    font-family: Inter;
+    font-size: 14px;
+    font-style: normal;
+    font-weight: 500;
+    line-height: 24px;
+`;
+
+export const WrapperUser = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    gap: 16px;
+`;
+
+export const WrapperAvatar = styled.div`
+    display: flex;
+    width: 260px;
+    padding: 8.813px 24.445px 0px 24.45px;
+    justify-content: center;
+    align-items: center;
+    border-radius: 100000000px;
+    border: 5px solid #033059;
+    background: #B9C1CA;
+`;
+
+export const ConteinerText = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    gap: 16px;
+`;
+
+export const TitleH2 = styled.h2`
+    color: var(--slate-900, #023059);
+    /* h2 */
+    font-family: Inter;
+    font-size: 30px;
+    font-style: normal;
+    font-weight: 600;
+    line-height: 36px; /* 120% */
+    letter-spacing: -0.225px;
+`;
+
+export const subtle = styled.div`
+    width: 377px;
+    color: #033059;
+
+    /* subtle-medium */
+    font-family: Inter;
+    font-size: 14px;
+    font-style: normal;
+    font-weight: 500;
+    line-height: 20px; /* 142.857% */
+`;
+
+export const Input = styled.input`
+    display: flex;
+    padding: 8px 12px;
+    align-items: center;
+    flex: 1 0 0;
+    border-radius: 6px;
+    border: 1px solid #CBD5E1;
+    background: #FFF;
+    width: 100%;
+    color: #0F172A;
+
+    font-family: Inter;
+    font-size: 14px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 20px; /* 142.857% */
+`;
+export const ContainerButtonCriarSala = styled.div`
+    display: flex;
+width: 377px;
+justify-content: flex-end;
+align-items: center;
+gap: 8px;
+`;
+export const ButtonCriarSala = styled.button`
+    display: flex;
+padding: 8px 16px;
+justify-content: center;
+align-items: center;
+gap: 10px;
+border-radius: 6px;
+background: var(--slate-900, #023059);
+color: #FFF;
+font-family: Inter;
+font-size: 14px;
+font-style: normal;
+font-weight: 500;
+line-height: 24px; /* 171.429% */
+`;
