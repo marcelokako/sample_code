93a047a6453d778b0a2102defc74ec6c7d49002b
lisperey <pilrss16@gmail.com>
2024-01-26 22:08:36 -0300

    Finalizado css

--- Front-end/src/pages/Home/index.jsx
+++ Front-end/src/pages/Home/index.jsx
@@ -1,45 +1,75 @@
 import React, { memo } from "react";
 import * as S from './styles';
 import logo from './../../assets/Logo-symphonIA.svg'
-
+import SD from './../../assets/Seta-direita.svg'
+import SE from './../../assets/Seta-esquerda.svg'
+import Avatar from './../../assets/avatar-cacti-cactus-svgrepo-com.svg'
 // import Icon from "./Icon.jsx"
 const Home = () => {
+
+  const ButtonCriarSala = () =>{
+    console.log('criar sala');
+  }
+  const ButtonSetaDireita = () =>{
+    console.log('seta direita');
+  }
   
     return (
       <S.Conteiner>
+
       <S.WrapperLogo>
         <img src={logo} alt="logo"></img>
       </S.WrapperLogo>
+
       <S.WrapperMain>
+
         <S.WrapperAvatar>
           
+          <S.Avatar>
+            <img src={Avatar}  alt="avatar"></img>
+          </S.Avatar>
+
+          <S.ContainerSetas>
+            <S.Seta src={SE} alt="logbotão esquerda"/>
+            <S.Seta onClick={ButtonSetaDireita} src={SD} alt="botão direita"/>
+          </S.ContainerSetas>
+          
         </S.WrapperAvatar>
+
         <S.WrapperUser>
         <S.ConteinerText>
             <S.TitleH2>
             Perfil anônimo
             </S.TitleH2>
-            <S.subtle>
+            <S.Subtle>
             Escolha seu username e seu avatar e comece a jogar!
-            </S.subtle>
+            </S.Subtle>
             <S.Input/>
             <S.ContainerButtonCriarSala>
-              <S.ButtonCriarSala>
+              <S.ButtonCriarSala onClick={ButtonCriarSala}>
                 Criar Sala
               </S.ButtonCriarSala>
             </S.ContainerButtonCriarSala>
           </S.ConteinerText>
           
         </S.WrapperUser>
+
       </S.WrapperMain>
+
       <S.WrapperAction>
+
+        <a href="/waiting-room">
         <S.ButtonLogar>
-          Logar
+            Logar
         </S.ButtonLogar>
+        </a>
+
         <S.ButtonCadastrar>
           Cadastre-se
         </S.ButtonCadastrar>
+
       </S.WrapperAction>
+
       </S.Conteiner>
     );
   }
--- Front-end/src/pages/Home/styles.js
+++ Front-end/src/pages/Home/styles.js
@@ -2,12 +2,12 @@ import styled from 'styled-components';
 
 export const Conteiner = styled.div`
     display: flex;
-    width: 1441px;
+    width: 100%;
     height: 100%;
     flex-shrink: 0;
     padding: 24px;
     flex-direction: column;
-    justify-content: start; /* Alterado de center para flex-start */
+    justify-content: center; /* Alterado de center para flex-start */
     align-items: center;
     
 
@@ -15,7 +15,7 @@ export const Conteiner = styled.div`
 
 export const WrapperLogo = styled.div`
     
-   width: 903px;
+    width: 903px;
     height: 162px;
     flex-shrink: 0;
      
@@ -64,6 +64,7 @@ export const ButtonLogar = styled.button`
     font-style: normal;
     font-weight: 500;
     line-height: 24px; 
+    cursor: pointer;
 `;
 export const ButtonCadastrar = styled.button`
     display: inline-flex;
@@ -80,6 +81,7 @@ export const ButtonCadastrar = styled.button`
     font-style: normal;
     font-weight: 500;
     line-height: 24px;
+    cursor: pointer;
 `;
 
 export const WrapperUser = styled.div`
@@ -92,13 +94,46 @@ export const WrapperUser = styled.div`
 export const WrapperAvatar = styled.div`
     display: flex;
     width: 260px;
+    height: 260px;
+    flex-direction: column;
+    align-items: center;
+
+`;
+
+export const Avatar = styled.div`
+    display: flex;
+    width: 260px;
+    height: 260px;
     padding: 8.813px 24.445px 0px 24.45px;
     justify-content: center;
     align-items: center;
     border-radius: 100000000px;
     border: 5px solid #033059;
     background: #B9C1CA;
+    flex-shrink: 0;
+    
 `;
+
+export const ContainerSetas = styled.div`
+    width: 260px;
+    height: 260px;
+    display: flex;
+    flex-direction: row;
+    align-items: end;
+    justify-content: space-between;
+    z-index: 1;
+    position: absolute;
+    top: 255px;
+
+`;
+
+export const Seta = styled.img`
+    cursor: pointer;
+    width: 46.667px;
+    height: 46.667px;
+    flex-shrink: 0;
+`;
+
 
 export const ConteinerText = styled.div`
     display: flex;
@@ -118,7 +153,7 @@ export const TitleH2 = styled.h2`
     letter-spacing: -0.225px;
 `;
 
-export const subtle = styled.div`
+export const Subtle = styled.b`
     width: 377px;
     color: #033059;
 
@@ -149,23 +184,25 @@ export const Input = styled.input`
 `;
 export const ContainerButtonCriarSala = styled.div`
     display: flex;
-width: 377px;
-justify-content: flex-end;
-align-items: center;
-gap: 8px;
+    width: 377px;
+    justify-content: flex-end;
+    align-items: center;
+    gap: 8px;
+
 `;
 export const ButtonCriarSala = styled.button`
     display: flex;
-padding: 8px 16px;
-justify-content: center;
-align-items: center;
-gap: 10px;
-border-radius: 6px;
-background: var(--slate-900, #023059);
-color: #FFF;
-font-family: Inter;
-font-size: 14px;
-font-style: normal;
-font-weight: 500;
-line-height: 24px; /* 171.429% */
+    padding: 8px 16px;
+    justify-content: center;
+    align-items: center;
+    gap: 10px;
+    border-radius: 6px;
+    background: var(--slate-900, #023059);
+    color: #FFF;
+    font-family: Inter;
+    font-size: 14px;
+    font-style: normal;
+    font-weight: 500;
+    line-height: 24px; /* 171.429% */
+    cursor: pointer;
 `;
